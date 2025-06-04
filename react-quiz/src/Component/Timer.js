421ef8e00ad21a50db6ef200d381c039a334a28e
milindpawar007 <milindpawar5555@gmail.com>
2025-06-04 14:19:17 -0500

    feat: enhance Timer component to display formatted minutes and seconds

--- react-quiz/src/Component/Timer.js
+++ react-quiz/src/Component/Timer.js
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
 
 function Timer({ dispatch, secondsRemaing }) {
+  const mins = Math.floor(secondsRemaing / 60);
+  const second = secondsRemaing % 60;
+
   useEffect(() => {
     const id = setInterval(() => {
       dispatch({ type: 'tick' });
@@ -11,7 +14,13 @@ function Timer({ dispatch, secondsRemaing }) {
     };
   }, [dispatch]);
 
-  return <div className="timer">{secondsRemaing}</div>;
+  return (
+    <div className="timer">
+      {mins < 10 && '0'}
+      {mins}: {second < 10 && '0'}
+      {second}
+    </div>
+  );
 }
 
 export default Timer;
