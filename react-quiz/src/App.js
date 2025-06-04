376233815b668b5e9ae9934e1e2dc4baf96cb345
milindpawar007 <milindpawar5555@gmail.com>
2025-06-03 20:27:32 -0500

    feat: implement data fetching and state management in App component

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,7 +1,53 @@
+import { useEffect, useReducer } from 'react';
 import Header from './Component/Header';
 import Main from './Component/Main';
 
+const initialState = {
+  questions: [],
+  // 'ready' ,'active', 'error, 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFalied':
+      return {
+        ...state,
+        status: 'error',
+      };
+    default:
+      throw Error('error');
+  }
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('http://localhost:8000/questions');
+        if (!response.ok) {
+          dispatch({ type: 'dataFalied' });
+          console.error(`HTTP error! status: ${response.status}`);
+        }
+        const questions = await response.json();
+        dispatch({ type: 'dataReceived', payload: questions });
+      } catch (error) {
+        console.error('Fetch error:', error);
+        dispatch({ type: 'dataFalied' });
+      }
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="app">
       <Header />
