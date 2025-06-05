50bb864b306601747968ef20ef6e098585cf0498
milindpawar007 <milindpawar5555@gmail.com>
2025-06-05 00:33:35 -0500

    feat: update App component to include routing for Homepage, Product, and Pricing pages

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Product from "./pages/Product"
+import Pricing from "./pages/Pricing"
+import Homepage from "./pages/Homepage"
 function App() {
 
 
   return (
-    <div>
-      Worldwise
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="product" element={<Product />}></Route>
+        <Route path="pricing" element={<Pricing />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
