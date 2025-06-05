2a3c658d726ec53b3829ec51021150b281821206
milindpawar007 <milindpawar5555@gmail.com>
2025-06-05 00:27:29 -0500

    Initialize worldwise project with React and Vite setup
    - Created package.json with dependencies and scripts for development, build, linting, and preview.
    - Added Vite logo SVG to public directory.
    - Implemented main App component with basic structure.
    - Included React logo SVG in assets directory.
    - Set up main entry point (main.jsx) for React application.
    - Created Homepage, Pricing, and Product components with basic structure.
    - Configured Vite with React and ESLint plugins in vite.config.js.

---
+++ worldwise/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <div>
+      Worldwise
+    </div>
+  )
+}
+
+export default App
---
+++ worldwise/src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+function Homepage() {
+    return (
+        <div>
+            Worldwise
+        </div>
+    )
+}
+
+export default Homepage
