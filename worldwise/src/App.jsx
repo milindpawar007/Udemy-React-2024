08f0e75f3123839472435eeb8e8e87ff2f5bc50f
milindpawar007 <milindpawar5555@gmail.com>
2025-06-05 00:35:34 -0500

    feat: add PageNotFound component and update routing in App component

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Product from "./pages/Product"
 import Pricing from "./pages/Pricing"
 import Homepage from "./pages/Homepage"
+import PageNotFound from "./pages/PageNotFound"
 function App() {
 
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Homepage />}></Route>
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </BrowserRouter>
   )
