67fd8e72428f97286308a6f0104a4fc3154a144f
milindpawar007 <milindpawar5555@gmail.com>
2025-06-05 11:43:09 -0500

    Add styles and components for AppLayout, Homepage, Login, and Product pages
    - Created AppLayout.module.css for overall app styling.
    - Added Homepage.module.css with layout and background styles.
    - Implemented Login component with form handling and styles in Login.module.css.
    - Developed Product page styles in Product.module.css for layout and typography.

--- worldwise/src/pages/PageNotFound.jsx
+++ worldwise/src/pages/PageNotFound.jsx
@@ -1,11 +1,7 @@
-import PageNav from "../Component/PageNav"
-function PageNotFound() {
-    return (
-        <div>
-            <PageNav />
-            PageNotFound
-        </div>
-    )
+export default function PageNotFound() {
+  return (
+    <div>
+      <h1>Page not found 😢</h1>
+    </div>
+  );
 }
-
-export default PageNotFound
