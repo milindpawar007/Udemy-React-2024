f2afe10571a26ac2fad6e2cc62f244cf79b89e4b
milindpawar007 <milindpawar5555@gmail.com>
2025-06-09 19:36:51 -0500

    feat: integrate city markers into Map component and enable scroll wheel zoom

--- worldwise/src/Component/Map.jsx
+++ worldwise/src/Component/Map.jsx
@@ -3,24 +3,32 @@ import styles from "./Map.module.css";
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import { useState } from "react";
+import { useCities } from "../Context/CitiesContext";
+import Flag from './Flag';
 function Map() {
     const navigate = useNavigate();
-    const [mapPosition] = useState([32.7767, -96.7970])
+    const { cities } = useCities();
+    const [mapPosition] = useState([52.53586782505711, 13.376933665713324])
     //  const [serchParam, setSerchParam] = useSearchParams();
     //  const lat = serchParam.get("lat")
 
     return (
         <div className={styles.mapContainer} onClick={() => { navigate("form") }}>
-            <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={false}>
+            <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
                 />
-                <Marker position={mapPosition}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {
+                    cities.map((city) => (
+                        <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+                            <Popup>
+                                <span>  <Flag emoji={city.emoji} /> </span>
+                                <span>{city.cityName}</span>
+                            </Popup>
+                        </Marker>))
+                }
+
             </MapContainer>
         </div>
     )
