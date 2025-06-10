8895594c0bf2ac56ccc5e10462ca870872003ab8
milindpawar007 <milindpawar5555@gmail.com>
2025-06-09 23:56:00 -0500

    feat: refactor Map component to correctly update map position based on search parameters

--- worldwise/src/Component/Map.jsx
+++ worldwise/src/Component/Map.jsx
@@ -2,17 +2,24 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
-//import { useState } from "react";
+import { useState } from "react";
 import { useCities } from "../Context/CitiesContext";
 import Flag from './Flag';
+import { useEffect } from "react";
 function Map() {
     const navigate = useNavigate();
     const { cities } = useCities();
-    // const [mapPosition] = useState([52.53586782505711, 13.376933665713324])
+    const [mapPosition, setMapPosition] = useState([52.53586782505711, 13.376933665713324])
     const [searchParam] = useSearchParams();
-    const maplat = Number(searchParam.get("lat")) || 52.53586782505711;
-    const maplng = Number(searchParam.get("lng")) || 13.376933665713324;
+    // const maplat = Number(searchParam.get("lat")) || 52.53586782505711;
+    // const maplng = Number(searchParam.get("lng")) || 13.376933665713324;
+    const maplat = Number(searchParam.get("lat"));
+    const maplng = Number(searchParam.get("lng"));
 
+
+    useEffect(() => {
+        if (maplat && maplng) setMapPosition([maplat, maplng])
+    }, [maplat, maplng])
     return (
         <div className={styles.mapContainer} onClick={() => { navigate("form") }}>
             <MapContainer className={styles.map} center={[maplat, maplng]} zoom={6} scrollWheelZoom={true}>
@@ -29,7 +36,7 @@ function Map() {
                             </Popup>
                         </Marker >))
                 }
-                <ChangeCenter position={[maplat, maplng]} />
+                <ChangeCenter position={mapPosition} />
             </MapContainer>
         </div>
     )
