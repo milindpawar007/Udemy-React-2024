dfb079513f4c59933478304584ef46539799e363
milindpawar007 <milindpawar5555@gmail.com>
2025-06-09 19:45:53 -0500

    feat: update Map component to use search parameters for dynamic center positioning and add ChangeCenter function

--- worldwise/src/Component/Map.jsx
+++ worldwise/src/Component/Map.jsx
@@ -1,20 +1,21 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+//import { useState } from "react";
 import { useCities } from "../Context/CitiesContext";
 import Flag from './Flag';
 function Map() {
     const navigate = useNavigate();
     const { cities } = useCities();
-    const [mapPosition] = useState([52.53586782505711, 13.376933665713324])
-    //  const [serchParam, setSerchParam] = useSearchParams();
-    //  const lat = serchParam.get("lat")
+    // const [mapPosition] = useState([52.53586782505711, 13.376933665713324])
+    const [searchParam] = useSearchParams();
+    const maplat = Number(searchParam.get("lat")) || 52.53586782505711;
+    const maplng = Number(searchParam.get("lng")) || 13.376933665713324;
 
     return (
         <div className={styles.mapContainer} onClick={() => { navigate("form") }}>
-            <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+            <MapContainer className={styles.map} center={[maplat, maplng]} zoom={6} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
@@ -26,12 +27,20 @@ function Map() {
                                 <span>  <Flag emoji={city.emoji} /> </span>
                                 <span>{city.cityName}</span>
                             </Popup>
-                        </Marker>))
+                        </Marker >))
                 }
-
+                <ChangeCenter position={[maplat, maplng]} />
             </MapContainer>
         </div>
     )
 }
 
+
+function ChangeCenter({ position }) {
+    const map = useMap();
+    map.setView(position)
+
+    return null;
+
+}
 export default Map
