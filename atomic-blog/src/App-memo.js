e14027614a90972b78bdf4b9a8a1aa3942357d51
milindpawar007 <milindpawar5555@gmail.com>
2025-06-11 17:44:34 -0500

    memo hook is added

--- atomic-blog/src/App-memo.js
+++ atomic-blog/src/App-memo.js
@@ -1,7 +1,7 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { useEffect, useState, memo } from 'react';
 import { faker } from '@faker-js/faker';
 import { useMemo } from 'react';
-import { memo } from 'react';
+import { useCallback } from 'react';
 
 function createRandomPost() {
   return {
@@ -9,9 +9,6 @@ function createRandomPost() {
     body: faker.hacker.phrase(),
   };
 }
-
-//1] create new context
-const PostContext = createContext();
 
 function App() {
   const [posts, setPosts] = useState(() =>
@@ -30,12 +27,11 @@ function App() {
         )
       : posts;
 
-  function handleAddPost(post) {
+  const handleAddPost = useCallback(function handleAddPost(post) {
     setPosts((posts) => [post, ...posts]);
-  }
+  }, []);
 
   function handleClearPosts() {
-    console.log('dsaadsa');
     setPosts([]);
   }
 
@@ -46,54 +42,53 @@ function App() {
     },
     [isFakeDark]
   );
-  const value = useMemo(() => {
+  const ArchiveOption = useMemo(() => {
     return {
-      posts: searchedPosts,
-      onClearPosts: handleClearPosts,
-      onAddPost: handleAddPost,
-      searchQuery,
-      setSearchQuery,
+      show: false,
+      title: `Post archive in addition ot ${posts.length} main post`,
     };
-  }, [searchedPosts, searchQuery]);
+  }, [posts.length]);
   return (
-    //2] provide value to child component
-    <PostContext.Provider value={value}>
-      <section>
-        <button
-          onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-          className="btn-fake-dark-mode"
-        >
-          {isFakeDark ? '☀️' : '🌙'}
-        </button>
+    <section>
+      <button
+        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+        className="btn-fake-dark-mode"
+      >
+        {isFakeDark ? '☀️' : '🌙'}
+      </button>
 
-        <Header />
-        <Main />
-        <Archive />
-        <Footer />
-      </section>
-    </PostContext.Provider>
+      <Header
+        posts={searchedPosts}
+        onClearPosts={handleClearPosts}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
+      <Main posts={searchedPosts} onAddPost={handleAddPost} />
+      <Archive ArchiveOption={ArchiveOption} onAddPost={handleAddPost} />
+      <Footer />
+    </section>
   );
 }
 
-function Header() {
-  const { onClearPosts } = useContext(PostContext);
-
+function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
   return (
     <header>
       <h1>
         <span>⚛️</span>The Atomic Blog
       </h1>
       <div>
-        <Results />
-        <SearchPosts />
+        <Results posts={posts} />
+        <SearchPosts
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+        />
         <button onClick={onClearPosts}>Clear posts</button>
       </div>
     </header>
   );
 }
 
-function SearchPosts() {
-  const { searchQuery, setSearchQuery } = useContext(PostContext);
+function SearchPosts({ searchQuery, setSearchQuery }) {
   return (
     <input
       value={searchQuery}
@@ -103,30 +98,28 @@ function SearchPosts() {
   );
 }
 
-function Results() {
-  const { posts } = useContext(PostContext);
+function Results({ posts }) {
   return <p>🚀 {posts.length} atomic posts found</p>;
 }
 
-const Main = memo(function Main() {
+function Main({ posts, onAddPost }) {
   return (
     <main>
-      <FormAddPost />
-      <Posts />
+      <FormAddPost onAddPost={onAddPost} />
+      <Posts posts={posts} />
     </main>
   );
-});
+}
 
-function Posts() {
+function Posts({ posts }) {
   return (
     <section>
-      <List />
+      <List posts={posts} />
     </section>
   );
 }
 
-function FormAddPost() {
-  const { onAddPost } = useContext(PostContext);
+function FormAddPost({ onAddPost }) {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
 
@@ -155,8 +148,7 @@ function FormAddPost() {
   );
 }
 
-function List() {
-  const { posts } = useContext(PostContext);
+function List({ posts }) {
   return (
     <ul>
       {posts.map((post, i) => (
@@ -169,19 +161,18 @@ function List() {
   );
 }
 
-function Archive() {
-  const { onAddPost } = useContext(PostContext);
+const Archive = memo(function Archive({ ArchiveOption, onAddPost }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 10000 }, () => createRandomPost())
   );
 
-  const [showArchive, setShowArchive] = useState(false);
+  const [showArchive, setShowArchive] = useState(ArchiveOption.show);
 
   return (
     <aside>
-      <h2>Post archive</h2>
+      <h2>{ArchiveOption.title}</h2>
       <button onClick={() => setShowArchive((s) => !s)}>
         {showArchive ? 'Hide archive posts' : 'Show archive posts'}
       </button>
@@ -200,7 +191,7 @@ function Archive() {
       )}
     </aside>
   );
-}
+});
 
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
