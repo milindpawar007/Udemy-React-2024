9f131a39ccd85d15762e56c0e6d908fe38994adf
milindpawar007 <milindpawar5555@gmail.com>
2025-06-11 20:55:24 -0500

    inc dec function added on btton to adjust the timer

--- workout-timer/src/Calculator.js
+++ workout-timer/src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import clickSound from './ClickSound.m4a';
 
 function Calculator({ workouts, allowSound }) {
@@ -7,7 +7,8 @@ function Calculator({ workouts, allowSound }) {
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  const [duration, setDuration] = useState(0);
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
@@ -17,12 +18,31 @@ function Calculator({ workouts, allowSound }) {
     sound.play();
   };
 
+  useEffect(() => {
+    const newduration =
+      (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+    setDuration(newduration);
+  }, [number, sets, speed, durationBreak]);
+
+  function handelInc() {
+    setDuration((p) => Math.floor(p + 1));
+  }
+  function handelDec() {
+    setDuration((p) => (p > 1 ? Math.ceil(p - 1) : 0));
+  }
+
   return (
     <>
       <form>
         <div>
           <label>Type of workout</label>
-          <select value={number} onChange={(e) => setNumber(+e.target.value)}>
+          <select
+            value={number}
+            onChange={(e) => {
+              setNumber(+e.target.value);
+              playSound();
+            }}
+          >
             {workouts.map((workout) => (
               <option value={workout.numExercises} key={workout.name}>
                 {workout.name} ({workout.numExercises} exercises)
@@ -49,7 +69,10 @@ function Calculator({ workouts, allowSound }) {
             max="180"
             step="30"
             value={speed}
-            onChange={(e) => setSpeed(e.target.value)}
+            onChange={(e) => {
+              setSpeed(e.target.value);
+              playSound();
+            }}
           />
           <span>{speed} sec/exercise</span>
         </div>
@@ -60,19 +83,34 @@ function Calculator({ workouts, allowSound }) {
             min="1"
             max="10"
             value={durationBreak}
-            onChange={(e) => setDurationBreak(e.target.value)}
+            onChange={(e) => {
+              setDurationBreak(e.target.value);
+              playSound();
+            }}
           />
           <span>{durationBreak} minutes/break</span>
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button
+          onClick={() => {
+            handelDec();
+          }}
+        >
+          –
+        </button>
         <p>
           {mins < 10 && '0'}
           {mins}:{seconds < 10 && '0'}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button
+          onClick={() => {
+            handelInc();
+          }}
+        >
+          +
+        </button>
       </section>
     </>
   );
