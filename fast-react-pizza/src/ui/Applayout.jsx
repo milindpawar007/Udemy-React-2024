a2b85a777d7ff0dfd2cd65b91fbae33f0dc9711e
milindpawar007 <milindpawar5555@gmail.com>
2025-06-14 15:50:16 -0500

    adder iloader compenenet while fecthing data

--- fast-react-pizza/src/ui/Applayout.jsx
+++ fast-react-pizza/src/ui/Applayout.jsx
@@ -1,13 +1,17 @@
 import React from "react"
 import Header from "./Header"
 import CartOverview from "../features/carts/CartOverview"
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigation } from "react-router-dom"
+import Laoder from "./Laoder";
 function Applayout() {
+    const navigation = useNavigation();
+    const isLoading = navigation.state === "loading"
     return (
-        <div>
+        <div className="layout">
+            {isLoading && <Laoder />}
             <Header />
             <main>
-                <h1>Content</h1>
+
                 <Outlet />
             </main>
             <CartOverview />
---
+++ fast-react-pizza/src/ui/Laoder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Laoder() {
+    return (
+        <div className='loader'>
+
+        </div>
+    )
+}
+
+export default Laoder
