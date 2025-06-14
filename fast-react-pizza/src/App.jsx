d3a7c16ec4c411c5f777d4769e641f9d1af30504
milindpawar007 <milindpawar5555@gmail.com>
2025-06-14 14:04:47 -0500

    feature :added some router app

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import "./index.css"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./ui/Home"
+import Menu from "./features/menu/Menu"
+import Order from './features/orders/Order';
+import CreateOrder from './features/orders/CreateOrder';
+
+import Cart from "./features/carts/Cart"
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <Order /> },
+  { path: "/menu", element: <Menu /> },
+
+])
+
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <RouterProvider router={router}>
+
+    </RouterProvider>
   )
 }
 
