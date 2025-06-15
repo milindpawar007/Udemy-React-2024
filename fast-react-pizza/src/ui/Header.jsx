a61f3207a5ab00bba8e63a2b5aef75edeed7df65
milindpawar007 <milindpawar5555@gmail.com>
2025-06-14 19:02:44 -0500

    feature added serch function

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import SearchOrder from '../features/orders/searchOrder'
 function Header() {
     return (
         <header>
             <Link to="/">Fast React Pizz co.</Link>
+            <SearchOrder />
             <p>Milind Pawar</p>
         </header>
     )
