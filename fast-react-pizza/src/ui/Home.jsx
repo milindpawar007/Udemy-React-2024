ef26f379aa83feb14bf35fe83c3ab8b4e05efa3e
milindpawar007 <milindpawar5555@gmail.com>
2025-06-15 00:36:23 -0500

    feature :added tailwind config

--- fast-react-pizza/src/ui/Home.jsx
+++ fast-react-pizza/src/ui/Home.jsx
@@ -2,9 +2,10 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>
+      <h1 className="text-xl text-yellow-500 font-semibold">
         The best pizza.
         <br />
+
         Straight out of the oven, straight to you.
       </h1>
     </div>
