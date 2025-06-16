a1aa122e3f226caa978ab7d516d399eef05f6c75
milindpawar007 <milindpawar5555@gmail.com>
2025-06-16 00:38:14 -0500

    added some css to order new page

--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 function Button({ children, disabled, to, type = 'primary' }) {
     console.log(type)
     const base =
-        'bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
+        'bg-yellow-400 text-sm uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
 
     const styles = {
         primary: `${base} px-4 py-4 md:px-6 sm:py-4`,
         small: `${base} px-4 py-2 md:px-5 md:py-2.5 text-xs`,
-        secondary: `border-2 border-stone-300 uppercase font-semibold text-stone-800 
+        secondary: `border-2 text-sm border-stone-300 uppercase font-semibold text-stone-800 
         inline-block tracking-wide rounded-full hover:bg-stone-300 
         hover:text-stone-800  focus:text-stone-800 transition-colors
         duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 
